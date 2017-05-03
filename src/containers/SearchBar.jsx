040e6e5f9ff1d5b36cec6b1c64e7039a96767669
Eric Pan <epan@users.noreply.github.com>
2017-05-02 22:11:37 -0700

    (feat) Make input field a controlled component

--- src/containers/SearchBar.jsx
+++ src/containers/SearchBar.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor () {
+    super();
+
+    this.state = { term: '' };
+  }
 
   render () {
     return (
       <form className="input-group">
-        <input type="text" />
+        <input
+          type="text"
+          placeholder="Enter city name for five-day forecast"
+          value={this.state.term}
+          onChange={e => this.setState({ term: e.target.value })}
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Get weather</button>
         </span>
