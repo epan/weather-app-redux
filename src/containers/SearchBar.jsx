d16061448a0b928847ddc6f141ec0741e24d6ec1
Eric Pan <epan@users.noreply.github.com>
2017-05-02 22:22:32 -0700

    (feat) Handle input and submit with methods

--- src/containers/SearchBar.jsx
+++ src/containers/SearchBar.jsx
@@ -5,16 +5,28 @@ export default class SearchBar extends Component {
     super();
 
     this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange (e) {
+    this.setState({ term: e.target.value });
+  }
+
+  onFormSubmit (e) {
+    e.preventDefault();
   }
 
   render () {
     return (
-      <form className="input-group">
+      <form className="input-group" onSubmit={this.onFormSubmit}>
         <input
+          className="form-control"
           type="text"
           placeholder="Enter city name for five-day forecast"
           value={this.state.term}
-          onChange={e => this.setState({ term: e.target.value })}
+          onChange={this.onInputChange}
         />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Get weather</button>
