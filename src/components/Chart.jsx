52ed1ab4beaa3f4db949bf06ba28014d686bd1e5
Eric Pan <epan@users.noreply.github.com>
2017-05-03 14:14:07 -0700

    (reafactor) Shorten syntax on chart component

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { Sparklines, SparklinesLine } from 'react-sparklines';
 
-export default (props) => {
-  return (
-    <div>
-      <Sparklines height={120} width={180} data={props.data}>
-        <SparklinesLine color={props.color} />
+export default ({data, color}) => (
+      <Sparklines height={120} width={180} data={data}>
+        <SparklinesLine color={color} />
       </Sparklines>
-    </div>
-  );
-};
+);
